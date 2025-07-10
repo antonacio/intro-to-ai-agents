f21ffd6384edb0c98c1ce7cd5f7f46e96259dd22
Pedro Antonacio <p.antonacio@gmail.com>
2025-07-09 23:11:47 -0300

    add reasearch agent to LangGraph Studio

---
+++ src/examples/agents/researcher/langgraph_studio.py
@@ -0,0 +1,6 @@
+from examples.agents.researcher.agent import ResearchAgent
+from examples.config import llm
+
+# Research agent
+research_agent = ResearchAgent(llm=llm)
+research_agent_compiled_graph = research_agent.build_graph().compile()
