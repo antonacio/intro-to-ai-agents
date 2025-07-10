798daffd43fbd913732f68f1e769333f4591ffdd
Pedro Antonacio <p.antonacio@gmail.com>
2025-07-10 19:57:58 -0300

    separate schema definitions from the agent class script

--- src/examples/agents/researcher/agent.py
+++ src/examples/agents/researcher/agent.py
@@ -1,13 +1,6 @@
 from typing import Literal
 from langchain_core.language_models import BaseLanguageModel
-from langchain_core.documents import Document
 from langgraph.graph import END, START, StateGraph
-from typing_extensions import Annotated
-from pydantic import BaseModel, Field
-from operator import add
-from langchain_core.messages import AnyMessage
-from langgraph.graph.message import add_messages
-
 
 from examples.agents.base_agent import BaseAgent
 from examples.agents.researcher.prompts import (
@@ -17,57 +10,13 @@ from examples.agents.researcher.prompts import (
     RESPOND_TO_USER_SYSTEM_PROMPT,
     RESPOND_WITH_RESEARCH_SYSTEM_PROMPT,
 )
+from examples.agents.researcher.schemas import (
+    InputState,
+    ResearchState,
+    ResearchPlan,
+    UserQueryClassification,
+)
 from examples.agents.retriever.agent import RetrieverAgent
-
-
-class UserQueryClassification(BaseModel):
-    """Schema used to classify the user's query."""
-
-    reasoning: str = Field(
-        description="Short sentence explaining the reasoning behind the classification of the user's query."
-    )
-    classification: Literal[
-        "ask_for_more_info", "conduct_research", "respond_to_user"
-    ] = Field(description="The classification of the user's query.")
-
-
-class ResearchPlan(BaseModel):
-    """Schema used to generate a research plan."""
-
-    steps: list[str] = Field(description="A list of steps in the research plan.")
-
-
-class InputState(BaseModel):
-    """Input state for the Research Agent graph."""
-
-    messages: Annotated[list[AnyMessage], add_messages] = Field(
-        description="The chat history between the user and the agent."
-    )
-
-
-class ResearchState(BaseModel):
-    """State of the Research Agent graph"""
-
-    messages: Annotated[list[AnyMessage], add_messages] = Field(
-        description="The chat history between the user and the agent."
-    )
-    query_classification: UserQueryClassification = Field(
-        default=None, description="The classification of the user's query."
-    )
-    research_steps: list[str] = Field(
-        default=[], description="A list of steps in the research plan."
-    )
-    current_step: int = Field(
-        default=0, description="Index of the current step in the research plan."
-    )
-    documents: Annotated[list[Document], add] = Field(
-        default=[],
-        description="This is a list of documents that the agent can reference to answer the user's query.",
-    )
-    research_results: str = Field(
-        default="",
-        description="The research results that will be used to answer the user's query.",
-    )
 
 
 class ResearchAgent(BaseAgent):
